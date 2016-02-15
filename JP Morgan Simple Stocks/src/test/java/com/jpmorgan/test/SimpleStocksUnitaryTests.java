41724fad014b2a6d5cc7eabbae98c90187b55ef7
Rafael Soledade Matos <rafaelsoledadem@gmail.com>
2016-02-15 00:31:52 +0000

    Project structure review

---
+++ JP Morgan Simple Stocks/src/test/java/com/jpmorgan/test/SimpleStocksUnitaryTests.java
@@ -0,0 +1,14 @@
+package test.java.com.jpmorgan.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleStocksUnitaryTests {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
