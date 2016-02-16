58e6542887a8584971c155a239d7ec5039cf582a
Rafael Soledade Matos <rafaelsoledadem@gmail.com>
2016-02-16 00:00:37 +0000

    Refactoring

---
+++ JP Morgan Simple Stocks/src/main/java/com/jpmorgan/simplestocks/supportedOperations/IStockOperations.java
@@ -0,0 +1,75 @@
+package main.java.com.jpmorgan.simplestocks.supportedOperations;
+
+import java.util.List;
+
+import main.java.com.jpmorgan.simplestocks.enums.TradeIndicator;
+import main.java.com.jpmorgan.simplestocks.model.Stock;
+import main.java.com.jpmorgan.simplestocks.model.StockTrade;
+
+/**
+ * Interface for the stock calculator. These are considered to be the base
+ * operations for all stocks.
+ * 
+ */
+public interface IStockOperations<T extends Stock> {
+
+    /**
+     * Exercise a. i.
+     * 
+     * @param stock
+     *            the stock being evaluated
+     * @param marketPrice
+     *            the established market price
+     * 
+     * @return the stock dividend yield
+     */
+    public double calculateDividendYield(T stock, double marketPrice);
+
+    /**
+     * Exercise a. ii.
+     * 
+     * @param stock
+     *            the stock being evaluated
+     * @param marketPrice
+     *            the established market price
+     * 
+     * @return the stock Price/Earnings ratio
+     */
+    public double calculatePeRatio(T stock, double marketPrice);
+
+    /**
+     * Exercise a. iii.
+     * 
+     * @param stock
+     *            the stock being transacted
+     * @param nShares
+     *            number of shares traded
+     * @param tradeIndicator
+     *            indication if it's a sell or buy trade
+     * @param tradePrice
+     *            the trade price
+     * 
+     * @return a stock trade record object
+     */
+    public StockTrade recordTrade(T stock, int nShares, TradeIndicator tradeIndicator, double tradePrice);
+
+    /**
+     * Exercise a. iv.
+     * 
+     * @param gbceStock
+     * @param timeSpanInMinutes
+     *            calculate trades for the past n minutes
+     * 
+     * @return the stocks volume weighted stock price
+     */
+    public double calculateVolumeWeightedStockPrice(T stock, int timeSpanInMinutes);
+
+    /**
+     * Exercise b.
+     * 
+     * @param allStocks
+     *            The list of all available stocks
+     * @return GBCE Price share index
+     */
+    public double calculateGBCEAllShareIndex(List<T> allStocks);
+}
---
+++ JP Morgan Simple Stocks/src/main/java/com/jpmorgan/simplestocks/supportedOperations/impl/GBCEStockOperations.java
@@ -0,0 +1,106 @@
+package main.java.com.jpmorgan.simplestocks.supportedOperations.impl;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import main.java.com.jpmorgan.simplestocks.enums.StockType;
+import main.java.com.jpmorgan.simplestocks.enums.TradeIndicator;
+import main.java.com.jpmorgan.simplestocks.model.GBCEStock;
+import main.java.com.jpmorgan.simplestocks.model.StockTrade;
+import main.java.com.jpmorgan.simplestocks.supportedOperations.IStockOperations;
+import main.java.com.jpmorgan.simplestocks.utils.DateHelper;
+
+/**
+ * Class responsible for doing all required GBCE stock calculation operations
+ * 
+ * @author rafaelmatos
+ * 
+ *         Logger
+ * 
+ *         JUNIT com testes de unidade: sanity checks, inserir dados de teste
+ *         errados (stock diferente), etc
+ * 
+ */
+public class GBCEStockOperations implements IStockOperations<GBCEStock> {
+
+    private static final double defaultReturnValue = 0.0;
+
+    public double calculateDividendYield(GBCEStock stock, double marketPrice) {
+
+	stock.setMarketPrice(marketPrice);
+
+	if (marketPrice > 0) {
+	    if (StockType.COMMON.equals(stock.getStockType())) {
+
+		return new BigDecimal((stock.getLastDividend() / marketPrice)).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+	    }
+
+	    if (StockType.PREFERRED.equals(stock.getStockType())) {
+		return new BigDecimal(((stock.getFixedDividend() * stock.getParValue()) / marketPrice)).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+	    }
+	}
+
+	return defaultReturnValue;
+    }
+
+    public double calculatePeRatio(GBCEStock stock, double marketPrice) {
+
+	if (stock.getLastDividend() > 0) {
+	    stock.setMarketPrice(marketPrice);
+	    return new BigDecimal((marketPrice / stock.getLastDividend())).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	return defaultReturnValue;
+    }
+
+    public StockTrade recordTrade(GBCEStock stock, int nShares, TradeIndicator tradeIndicator, double tradePrice) {
+
+	StockTrade stockTrade = new StockTrade();
+
+	if (nShares * tradePrice > 0) {
+
+	    stockTrade.setQuantity(nShares);
+	    stockTrade.setTimeStamp(new Date());
+	    stockTrade.setTradeIndicator(tradeIndicator);
+	    
+	    tradePrice = new BigDecimal(tradePrice).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+	    stockTrade.setTradePrice(tradePrice);
+
+	    stock.getTradeRecords().add(stockTrade);
+	}
+	return stockTrade;
+    }
+
+    public double calculateVolumeWeightedStockPrice(GBCEStock gbceStock,
+	    int timeSpanInMinutes) {
+
+	double divisor = 0.0;
+	double divider = 0.0;
+
+	for (StockTrade currentTrade : gbceStock.getTradeRecords()) {
+
+	    if (DateHelper.isTradeProcessedWithinTimeSpan(currentTrade.getTimeStamp(), timeSpanInMinutes)) {
+		divisor += currentTrade.getQuantity();
+		divider += currentTrade.getTradePrice() * currentTrade.getQuantity();
+	    }
+	}
+	if (divisor > 0) {
+	    return new BigDecimal(divider / divisor).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	return defaultReturnValue;
+    }
+
+    public double calculateGBCEAllShareIndex(List<GBCEStock> allStocks) {
+
+	double stockPriceMultiplied = 1.0;
+
+	if (allStocks.size() > 0) {
+	    for (GBCEStock currStock : allStocks) {
+		stockPriceMultiplied *= currStock.getMarketPrice();
+	    }
+	    return new BigDecimal(Math.pow(stockPriceMultiplied, 1.0 / allStocks.size())).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	return defaultReturnValue;
+    }
+}
